f394c6c41d4f5c37f2661236345effc60926556f
monycky <monyckyfrazao@gmail.com>
2018-08-15 19:08:15 -0300

    bit change on structure of the project

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './app/layout/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const rootEl = document.getElementById('root');
+
+let render = () =>{
+    ReactDOM.render(<App />, rootEl)
+}
+
+
+if(module.hot){
+    module.hot.accept('./App', () =>{
+        setTimeout(render)
+    });
+}
+
+render();
 registerServiceWorker();
