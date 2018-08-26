1a0d6a62863be471f0d3c00f948083beccb8d73c
monycky <monyckyfrazao@gmail.com>
2018-08-26 19:57:22 -0300

    add event list, item and attendee

---
+++ src/features/events/EventList/EventListAtendee.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class EventListAtendee extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default EventListAtendee
